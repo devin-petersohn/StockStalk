a3066b0362d0463b6b7336afd23a7a64ac23750f
Devin Petersohn <devin.petersohn@gmail.com>
2016-02-18 14:01:09 -0600

    Added Spark capabilities with Yahoo Finance API

--- StockStalk_Maven/src/main/java/ScalaTest.scala
+++ StockStalk_Maven/src/main/java/ScalaTest.scala
@@ -1,9 +1,14 @@
+import java.util.Calendar
+import collection.JavaConversions._
+
 import yahoofinance.Stock
 import yahoofinance.YahooFinance
 import yahoofinance.histquotes.HistoricalQuote
 import yahoofinance.histquotes.Interval
+
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks._
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
@@ -19,5 +24,11 @@ object ScalaTest {
     val sc = new SparkContext(new SparkConf().setAppName("Testing_Scala").setMaster("local[4]"))
     val x = sc.parallelize(Array(1,2,3,4,5,6,7,8,9,10))
     x.collect().foreach(println)
+
+    val from = Calendar.getInstance();
+    from.add(Calendar.YEAR, -1)
+    val stock_history = sc.parallelize(stock.getHistory(from, Interval.DAILY))
+    println(Interval.DAILY)
+    println(stock_history.first())
   }
 }
