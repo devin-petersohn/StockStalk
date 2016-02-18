0fea9d1b505823ed485f254e8ccd2185da6ba46a
Devin Petersohn <devin.petersohn@gmail.com>
2016-02-18 13:53:07 -0600

    Testing changes

--- StockStalk_Maven/src/main/java/ScalaTest.scala
+++ StockStalk_Maven/src/main/java/ScalaTest.scala
@@ -2,6 +2,11 @@ import yahoofinance.Stock
 import yahoofinance.YahooFinance
 import yahoofinance.histquotes.HistoricalQuote
 import yahoofinance.histquotes.Interval
+import scala.collection.mutable.ArrayBuffer
+import scala.util.control.Breaks._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 
 object ScalaTest {
   def main(args: Array[String]) {
@@ -11,5 +16,8 @@ object ScalaTest {
     if (!(stock.getName == "N/A")) {
       println(stock.getSymbol + " - " + stock.getName + " => $" + stock.getQuote.getPrice + " (" + stock.getQuote.getChangeInPercent + "%)")
     }
+    val sc = new SparkContext(new SparkConf().setAppName("Testing_Scala").setMaster("local[4]"))
+    val x = sc.parallelize(Array(1,2,3,4,5,6,7,8,9,10))
+    x.collect().foreach(println)
   }
 }
