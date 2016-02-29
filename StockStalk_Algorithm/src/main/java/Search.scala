db9e3475ceb958fbdb57485dc4fd5b28ceae84a7
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-02-28 19:32:55 -0600

    Adding example to other project

--- StockStalk_Algorithm/src/main/java/Search.scala
+++ StockStalk_Algorithm/src/main/java/Search.scala
@@ -3,7 +3,14 @@ import net.sf.javaml.core.SparseInstance
 import net.sf.javaml.distance.dtw.DTWSimilarity
 import net.sf.javaml.distance.fastdtw.dtw._
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+
 object Search {
+  val sc = new SparkContext(new SparkConf().setAppName("Search").setMaster("local[4]"))
+
   def main(args: Array[String]) = {
     println("Hello World")
     val instanceOne: Instance = new SparseInstance(10)
@@ -16,6 +23,8 @@ object Search {
     instanceTwo.put(3, 4.0)
     val ds: DTWSimilarity = new DTWSimilarity
 //    val dtw: DTW = new DTW
+
+
 
     System.out.println(ds.measure(instanceOne, instanceTwo))
   }
