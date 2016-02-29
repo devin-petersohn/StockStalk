b060caaa7667bbb6a371dd3ea5becd6df4e04932
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-02-28 18:48:39 -0600

    Added example for saving file to scala

--- StockStalk_Maven/src/main/java/ScalaTest.scala
+++ StockStalk_Maven/src/main/java/ScalaTest.scala
@@ -1,3 +1,4 @@
+import java.util
 import java.util.concurrent.TimeUnit
 import java.util.{Date, GregorianCalendar, Calendar}
 import org.apache.spark.rdd.RDD
@@ -20,8 +21,22 @@ import org.apache.spark.rdd.RDD
 object ScalaTest {
   val sc = new SparkContext(new SparkConf().setAppName("Testing_Scala").setMaster("local[4]"))
 
+  val sANDp500 = Array("MMM","ABT","ABBV","ACN","ATVI","ADBE","ADT","AAP","AES","AET","AFL","AMG","A","GAS","APD","ARG","AKAM","AA","AGN","ALXN","ALLE","ADS","ALL","GOOGL","GOOG","MO","AMZN","AEE","AAL","AEP","AXP","AIG","AMT","AMP","ABC","AME","AMGN","APH","APC","ADI","AON","APA","AIV","AAPL","AMAT","ADM","AIZ","T","ADSK","ADP","AN","AZO","AVGO","AVB","AVY","BHI","BLL","BAC","BK","BCR","BXLT","BAX","BBT","BDX","BBBY","BRK-B","BBY","BIIB","BLK","HRB","BA","BWA","BXP","BSX","BMY","BF-B","CHRW","CA","CVC","COG","CAM","CPB","COF","CAH","HSIC","KMX","CCL","CAT","CBG","CBS","CELG","CNP","CTL","CERN","CF","SCHW","CHK","CVX","CMG","CB","CHD","CI","XEC","CINF","CTAS","CSCO","C","CTXS","CLX","CME","CMS","COH","KO","CCE","CTSH","CL","CPGX","CMCSA","CMA","CAG","CXO","COP","CNX","ED","STZ","GLW","COST","CCI","CSRA","CSX","CMI","CVS","DHI","DHR","DRI","DVA","DE","DLPH","DAL","XRAY","DVN","DO","DFS","DISCA","DISCK","DG","DLTR","D","DOV","DOW","DPS","DTE","DD","DUK","DNB","ETFC","EMN","ETN","EBAY","ECL","EIX","EW","EA","EMC","EMR","ENDP","ESV","ETR","EOG","EQT","EFX","EQIX","EQR","ESS","EL","ES","EXC","EXPE","EXPD","ESRX","EXR","XOM","FFIV","FB","FAST","FRT","FDX","FIS","FITB","FSLR","FE","FISV","FLIR","FLS","FLR","FMC","FTI","F","BEN","FCX","FTR","GME","GPS","GRMN","GD","GE","GGP","GIS","GM","GPC","GILD","GS","GT","GWW","HAL","HBI","HOG","HAR","HRS","HIG","HAS","HCA","HCP","HP","HES","HPE","HD","HON","HRL","HST","HPQ","HUM","HBAN","ITW","ILMN","IR","INTC","ICE","IBM","IP","IPG","IFF","INTU","ISRG","IVZ","IRM","JEC","JBHT","JNJ","JCI","JPM","JNPR","KSU","K","KEY","GMCR","KMB","KIM","KMI","KLAC","KSS","KHC","KR","LB","LLL","LH","LRCX","LM","LEG","LEN","LVLT","LUK","LLY","LNC","LLTC","LMT","L","LOW","LYB","MTB","MAC","M","MNK","MRO","MPC","MAR","MMC","MLM","MAS","MA","MAT","MKC","MCD","MHFI","MCK","MJN","WRK","MDT","MRK","MET","KORS","MCHP","MU","MSFT","MHK","TAP","MDLZ","MON","MNST","MCO","MS","MOS","MSI","MUR","MYL","NDAQ","NOV","NAVI","NTAP","NFLX","NWL","NFX","NEM","NWSA","NWS","NEE","NLSN","NKE","NI","NBL","JWN","NSC","NTRS","NOC","NRG","NUE","NVDA","ORLY","OXY","OMC","OKE","ORCL","OI","PCAR","PH","PDCO","PAYX","PYPL","PNR","PBCT","POM","PEP","PKI","PRGO","PFE","PCG","PM","PSX","PNW","PXD","PBI","PNC","RL","PPG","PPL","PX","CFG","PCLN","PFG","PG","PGR","PLD","PRU","PEG","PSA","PHM","PVH","QRVO","PWR","QCOM","DGX","RRC","RTN","O","RHT","REGN","RF","RSG","RAI","RHI","ROK","COL","ROP","ROST","RCL","R","CRM","SNDK","SCG","SLB","SNI","STX","SEE","SRE","SHW","SIG","SPG","SWKS","SLG","SJM","SNA","SO","LUV","SWN","SE","STJ","SWK","SPLS","SBUX","HOT","STT","SRCL","SYK","STI","SYMC","SYF","SYY","TROW","TGT","TEL","TE","TGNA","THC","TDC","TSO","TXN","TXT","HSY","TRV","TMO","TIF","TWX","TWC","TJX","TMK","TSS","TSCO","RIG","TRIP","FOXA","FOX","TSN","TYC","USB","UA","UNP","UAL","UNH","UPS","URI","UTX","UHS","UNM","URBN","VFC","VLO","VAR","VTR","VRSN","VRSK","VZ","VRTX","VIAB","V","VNO","VMC","WMT","WBA","DIS","WM","WAT","ANTM","WFC","HCN","WDC","WU","WY","WHR","WFM","WMB","WLTW","WEC","WYN","WYNN","XEL","XRX","XLNX","XL","XYL","YHOO","YUM","ZBH","ZION","ZTS")
   def main(args: Array[String]) {
+    var stocks = new ArrayBuffer[Stock]()
+    var history = new ArrayBuffer[(Stock,util.List[HistoricalQuote])]()
+    val from = new GregorianCalendar(2006, 0, 1)
+    val to = Calendar.getInstance
+    for(stock <- sANDp500) {
+      stocks += YahooFinance.get(stock)
+      history += ((YahooFinance.get(stock), YahooFinance.get(stock).getHistory(from, to, Interval.DAILY)))
+    }
+    sc.parallelize(history).saveAsObjectFile("/usr/devin/stocks")
+    var x = sc.objectFile[(Stock, util.List[HistoricalQuote])]("/usr/devin/stocks")
+    x = x.filter(stock => stock._1.getName == "Google")
+    /*
     println("Hello from Scala!")
+
     println("Hello World")
     val stock = YahooFinance.get("GOOG")
     val stock2 = YahooFinance.get("AAPL")
@@ -35,7 +50,7 @@ object ScalaTest {
     //percent_hist.saveAsObjectFile("data/DAILY")
     //val test = sc.objectFile[(Double, Long)]("data/WEEKLY")
 
-    var test = sc.union(convertPercentChange(percent_hist, 0.1), convertPercentChange(percent_hist2, 0.1)).map(_.swap)
+    var test = sc.union(convertPercentChange(percent_hist, 1.0), convertPercentChange(percent_hist2, 1.0)).map(_.swap)
     //val b = test.collect
     //b.foreach(println)
     var numDays = 1
@@ -52,6 +67,7 @@ object ScalaTest {
     }
 
     previous.collect.foreach(println)
+    */
     //TestClass.test()
   }
 
