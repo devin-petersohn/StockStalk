fbecabb46ef2ed4c45745a63ed538268fb731b51
Morgan <mmhkwc@mail.missouri.edu>
2016-03-03 22:26:04 -0600

    Fixed navbar bug with dropdowns on 3 subpages

--- chart.php
+++ chart.php
@@ -18,6 +18,17 @@
 	</ul>
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js" type="text/javascript" charset="utf-8"></script>
 	<script src="js/app.js" type="text/javascript" charset="utf-8"></script>
+    
+    
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+<!--<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>-->
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+
 </body>
 </html>
 
