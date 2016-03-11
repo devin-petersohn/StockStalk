ce468f12f052803fde95a9530b1f8ad0261c4c9a
Malcolm Bouchee <mabrm9@mail.missouri.edu>
2016-03-11 02:58:54 -0600

    Mock registration and login page created to test database interaction

--- logintest/index.php
+++ logintest/index.php
@@ -18,45 +18,37 @@
 	{
 		if (isset($_POST['submit']))
 		{
+			//connects user to database.
+			$dbconn = mysqli_connect("localhost", "mysql", "password","capstone");
+			or die("Could not connect: " . mysqli_connect_error());
 			//retrieve user input
 			$name=$_POST['username'] or die('Input is invalid');
 			$password=$_POST['password'] or die('Input is invalid');
-			/*$query='SELECT * FROM lab8.authentication WHERE username=$1';
+			$query='SELECT * FROM loginInfo WHERE username=?';
 			//Prepared statement
-			$result=pg_prepare($dbconn,'login',$query) or die("Query failed: " . pg_last_error());
-			$result=pg_execute($dbconn,'login',array($name)) or die("Query failed: " . pg_last_error());
-			$line = pg_fetch_array($result, null, PGSQL_ASSOC); 
+			$stmt=$mysqli->prepare($query) or die("Query failed");
+			$stmt->bind_param("s",$name);
+			$stmt->execute() or die ("Query failed");
+			$line = $stmt->fetch_array(MYSQLI_ASSOC); 
 			$salt=trim($line['salt']);
 			$hashpw=sha1($salt.$password);
 			
 			//check password
-			if($hashpw==$line['password_hash'])
+			if($hashpw==$line['hashpass'])
 			{
 				$_SESSION['user']=$name;
 				$_SESSION['loggedin']=true;
-				
-				//insert info into log table
-				$result=pg_prepare($dbconn, 'credentials', 'INSERT INTO lab8.log (username,ip_address, log_date, action) VALUES ($1, $2, $3, $4)') 
-				or die("Query failed: " . pg_last_error());
-				$result=pg_execute($dbconn, 'credentials', array($name, $_SERVER['REMOTE_ADDR'], date(DATE_RSS), 'login'))
-				or die("Query failed: " . pg_last_error());
-				//redirect to homepage
+
 				header('location: home.php');
 			}
 			else die('Incorrect username or password. Please try again.');
 			if($_SESSION['loggedin'] == true)
-				header('location: home.php');*/
-			if ($name=="username" && $password=="password")
-			{
-				$_SESSION['user']=$name;
-				$_SESSION['loggedin']=true;
-			}
-			else die('Incorrect username or password. Please try again.');
-			if($_SESSION['loggedin'] == true)
 				header('location: home.php');
+
 		}
-		
 	}
+	else header('location: index.php');
+	mysqli_close($dbconn);
 	?>
 </body>
 </html>
---
+++ logintest/registration.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>Registration Page</title>
+</head>
+<form method='POST' action='registration.php'>
+	<label for="username">username:</label>
+		  <input type="text" name="username" id="username">
+		  <label for="password">password:</label>
+		  <input type="password" name="password" id="password">
+		  <br>
+		  <input type="submit" name="submit" value="submit">
+</form>
+Return to <a href='index.php'>login</a> page.<br />
+
+<?php
+session_start();
+if($_SERVER['HTTPS'])
+{
+	if(isset($_POST['submit']))
+	{
+		//connects user to database.
+		$dbconn = mysqli_connect("localhost", "mysql", "password","capstone");
+		$name = $_POST['username'];
+		$password = $_POST['password'];
+		if($name == NULL || $password == NULL)
+			die('All fields required');
+	
+		//hash and salt password
+		mt_srand();
+		$salt = mt_rand();
+		$pwhash = sha1($salt.$password);
+		
+		//insert into loginInfo table
+		$query = 'INSERT INTO loginInfo (username, hashpass, salt) VALUES (?, ?, ?)';
+		$stmt=$mysqli->prepare($query) or die("Query failed");
+		$stmt->bind_param("sss",$name,$pwhash,$salt);
+		$stmt->execute() or die ("Query failed");
+		//redirect user to home page
+		header('location: home.php');
+	}
+	if($_SESSION['loggedin'])
+		header('location: home.php');
+}
+else
+	header('location: registration.php');
+?>
