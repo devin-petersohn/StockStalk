08ee89256684d8b710ee598ab29de34f8c260724
Sharon <xltz6@mail.missouri.edu>
2016-03-15 21:19:31 -0500

    add searchbox, market sector and date picker

--- search.php
+++ search.php
@@ -25,7 +25,93 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+    
+    <!-- jquery for datepicker -->
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    
+    <script>
+        $(function() {
+            $( "#from" ).datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                dateFormat: "yy-mm-dd",
+                numberOfMonths: 3,
+                maxDate: -2,
+                minDate: "-5y",
+                showOptions: { direction: "down" },
+                onClose: function( selectedDate ) {
+                $( "#to" ).datepicker( "option", "minDate", selectedDate );
+            }
+        });
+            $( "#to" ).datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                dateFormat: "yy-mm-dd",
+                numberOfMonths: 3,
+                maxDate: -1,
+                showOptions: { direction: "down" },
+                onClose: function( selectedDate ) {
+                $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+                }
+            });
+      });
+      </script>
 
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <br><br>
+    <div class="container">
+
+        <!-- Heading Row -->
+        <div class="row">
+            <form action="#" id="search">
+                 <!-- Search box -->
+                <div id="custom-search-input">
+                    <div class="input-group col-sm-6 col-sm-offset-3">
+                        <input type="text" class="search-query form-control" name="searchbox" placeholder="Search"></input>
+                            <span class="input-group-btn">
+                                <button style="height:34px;" class="btn btn-primary" type="submit">
+                                    <span class="glyphicon glyphicon-search"></span>
+                                </button>
+                            </span>
+
+                    </div>
+                </div>
+            </form>
+            <br>
+            <!-- select sector-->
+            <div class="input-group col-sm-6 col-sm-offset-3" form="search">
+                <h4>Market Sector</h4>
+                <select style="width:100%;height:25px; text-align: left;" name="sector" class="btn btn-default" type="button">
+                    <option value="All">All</option>
+                    <option value="Technology">Technology</option>
+                    <option value="Finance">Finance</option>
+                    <option value="Consumer">Consumer Goods</option>
+                    <option value="Automotive">Automotive</option>
+                    <option value="Conglomerates">Conglomerates</option>
+                    <option value="Energy">Energy</option>
+                    <option value="Utilities">Utilities</option>
+                </select>
+            </div>
+            <br>
+            <br>
+            <!-- choose time period-->
+            <div style="height:200px;" class="input-group col-sm-6 col-sm-offset-3" form="search">
+                <h4>Choose a time period</h4>
+                
+                <label for="from">From</label>
+                <input style="margin-left: 5px;" type="text" id="from" name="from">
+                <label style="margin-left: 10px;" for="to">to</label>
+                <input style="margin-left: 5px;" type="text" id="to" name="to">
+            </div>
+                
+        </div>
+    </div>
+    
 
 </body>
 </html>
