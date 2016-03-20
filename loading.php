d3c69c6c3ce0170e0b27bec94220f6d220deac33
Morgan <mmhkwc@mail.missouri.edu>
2016-03-20 14:45:41 -0500

    Loading page stuff

---
+++ loading.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Loading Results</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/login.css">
+<link rel="stylesheet" type="text/css" href="css/one-page-wonder.css">
+<link rel="stylesheet" type="text/css" href="css/charts.css">
+</head>
+<body>
+
+    <!---Navbar call----->
+    <?php include "navbar.html"; ?>
+    
+    
+	<h1>Search Your Stocks</h1>
+    
+    <center>
+          <img src="img/ldin.gif" width="150" height="150" alt="loading, please wait">
+    </center>
+
+    
+    
+    
+    
+    
+    
+	<script src="js/app.js" type="text/javascript" charset="utf-8"></script>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+<!--<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>-->
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
