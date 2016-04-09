e2839b23c47bcde5a9b55b126f33b0482400d500
Devin Petersohn <devin.petersohn@gmail.com>
2016-04-09 16:05:29 -0500

    Merge branch 'master' of https://github.com/devin-petersohn/StockStalk
    # Conflicts:
    #	.idea/workspace.xml

--- setup_and_deploy/setup.sh
+++ setup_and_deploy/setup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+MASTER='local'
+EXEC_MEM='7G'
+
+(cd All_Against_All && mvn install)
 (cd All_Against_All && mvn package)
+
+(cd Cache_Data && mvn install)
 (cd Cache_Data && mvn package)
+
+(cd One_Against_All && mvn install)
 (cd One_Against_All && mvn package)
 
-spark-submit
+
+
+spark-submit --master $MASTER --executor_memory $EXEC_MEM Cache_Data/target/cache_data-1.0-SNAPSHOT.jar INITIAL
