8cd259193172bdf65271ff2a30d7e63cec28a016
Madelyn Petersohn <mcs526@mail.missouri.edu>
2016-04-09 17:00:47 -0500

    autopopulate stocks in portfolio and make chart appear on page load

--- portfolio.php
+++ portfolio.php
@@ -13,6 +13,7 @@
 
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="js/colorbox/example1/colorbox.css" />
     <link rel="stylesheet" type="text/css" href="css/one-page-wonder.css">
 
 
@@ -27,92 +28,15 @@
     <script src="https://code.highcharts.com/stock/highstock.js"></script>
     <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
     
-    <script>
-        $(function () {
-        var seriesOptions = [],
-            seriesCounter = 0,
-            names = ['MSFT', 'AAPL', 'GOOG'];
-
-        /**
-         * Create the chart when all data is loaded
-         * @returns {undefined}
-         */
-        function createChart() {
-
-//            $('#container').highcharts('StockChart', {
-            var chart = new Highcharts.StockChart({
-                chart:{
-                    renderTo: 'container'
-                },
-//                
-                rangeSelector: {
-                    selected: 4
-                },
-
-                yAxis: {
-                    labels: {
-                        formatter: function () {
-                            return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                        }
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 2,
-                        color: 'silver'
-                    }]
-                },
-
-                plotOptions: {
-                    series: {
-                        compare: 'percent'
-                    }
-                },
-
-                tooltip: {
-                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                    valueDecimals: 2
-                },
-
-                series: seriesOptions
-            });
-        }
-
-        $.each(names, function (i, name) {
-
-            $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
-
-                seriesOptions[i] = {
-                    name: name,
-                    data: data
-                };
-
-                // As we're loading the data asynchronously, we don't know what order it will arrive. So
-                // we keep a counter and create the chart when all the data is loaded.
-                seriesCounter += 1;
-
-                if (seriesCounter === names.length) {
-                    createChart();
-                }
-            });
-        });
-    });
-    </script>
+    
 
 </head>
     
 
 
-<body>
-
-    
-    
-    
+<body onload="GetCellValues()">    
         <!---Navbar call----->
     <?php include "navbar.html"; ?>
-    
-    
-    
-    
 
     <!-- Page Content -->
     <div class="container">
@@ -123,6 +47,7 @@
             <!-- /.col-md-8 -->
             <div class="col-md-12">
                 <div class="well charts">
+                    <!--<p><a href="#" onClick="GetCellValues();">Top Text</a></p>-->
                     
 
 
@@ -140,14 +65,9 @@
             <div class="col-lg-12">
                  <div class="panel panel-default">
                     <div class="panel-heading">
+                        <h4>My Stocks</h4>
                         <div class="col-sm-4">
-                            <h4>My Stocks</h4>
-                        </div>
-                        <div class="align">
-                            <button type="button" class="btn btn-success btn-circle"><i class="fa fa-link">Add Stock +</i>
-                            </button>
-                            <button type="button" class="btn btn-danger btn-circle"><i class="fa fa-heart">Delete Stock -</i>
-                            </button>
+                            
                         </div>
                     </div>
                     <div class="panel-body">
@@ -165,10 +85,10 @@
                                         <th>Intraday</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="tableBody">
                                     <tr class="odd gradeX">
-                                        <td>BMW</td>
-                                        <td>BYERISCHE MOTOREN WERKE AG</td>
+                                        <td class="tick">BMW</td>
+                                        <td><a href="stockDetail.php" class="test-link">BYERISCHE MOTOREN WERKE AG</a></td>
                                         <td>1 Jan</td>
                                         <td>N/A</td>
                                         <td class="center">0.00$</td>
@@ -177,8 +97,28 @@
                                         <td class="center">X</td>
                                     </tr>
                                     <tr class="even gradeC">
-                                        <td>BMW.BE</td>
-                                        <td>BMW</td>
+                                        <td class="tick">AAPL</td>
+                                        <td><a href="stockDetail.php" class="test-link">Apple</a></td>
+                                        <td>11 Sep</td>
+                                        <td class="center">34.01</td>
+                                        <td class="center">0.53</td>
+                                        <td class="center">1.53%</td>
+                                        <td class="center">8924</td>
+                                        <td class="center">X</td>
+                                    </tr>
+                                    <tr class="odd gradeX">
+                                        <td class="tick">GOOG</td>
+                                        <td><a href="stockDetail.php" class="test-link">Google</a></td>
+                                        <td>1 Jan</td>
+                                        <td>N/A</td>
+                                        <td class="center">0.00$</td>
+                                        <td class="center">0.00%</td>
+                                        <td class="center">0</td>
+                                        <td class="center">X</td>
+                                    </tr>
+                                    <tr class="even gradeC">
+                                        <td class="tick">Z</td>
+                                        <td><a href="stockDetail.php" class="test-link">Zillow</a></td>
                                         <td>11 Sep</td>
                                         <td class="center">34.01</td>
                                         <td class="center">0.53</td>
@@ -196,6 +136,18 @@
         </div>
         <!-- /.row -->
 
+        <div style="display: none;">
+            <div id="test-content">
+                <div style="float:left; width:50%; padding: 10px;">
+                    <img src="img/stock.png" style="width:100%;">
+                </div>
+            <div style="float:left; width:50%; padding: 10px;">
+                <h1 style="color:black;">Stock Name: </h1>
+                <h1 style="color:black;">Stock Description: </h1>
+                <h1 style="color:black;">Misc Info: </h1>
+            </div>
+            </div>
+        </div>
         
 
         <!-- Footer -->
@@ -209,6 +161,122 @@
 
     </div>
     <!-- /.container -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="js/colorbox/jquery.colorbox-min.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+    $(".test-link").colorbox({width:"60%", height:"400px", inline:true, href:"#test-content"});
+</script>
+<script>
+function GetCellValues() {
+        /*$('#dataTables-example tr').each(function() {
+            var customerId = $(this).find(".test-link").html();  
+            alert(customerId);  
+        });*/
+    $(function () {
+            var tickername = [];
+            $('.tableBody tr').each(function (i) {
+                        var customerId = $(this).find(".tick").html();
+                        tickername.push(customerId);
+                
+            });
+            console.log(tickername);
+            
+        
+            var seriesOptions = [],
+            seriesCounter = 0,
+            names = tickername;
+            
+            var url = 'http://query.yahooapis.com/v1/public/yql';
+            var startDate = '2015-10-01';
+            var endDate = '2016-03-01';
+        /**
+         * Create the chart when all data is loaded
+         * @returns {undefined}
+         */
+            function createChart() {
+
+    //            $('#container').highcharts('StockChart', {
+                var chart = new Highcharts.StockChart({
+                    chart:{
+                        renderTo: 'container'
+                    },
+    //                
+                    rangeSelector: {
+                        selected: 4
+                    },
+
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                            }
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 2,
+                            color: 'silver'
+                        }]
+                    },
+
+                    plotOptions: {
+                        series: {
+                            compare: 'percent'
+                        }
+                    },
+
+                    tooltip: {
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        valueDecimals: 2
+                    },
+
+                    series: seriesOptions
+                });
+            }
+
+            $.each(names, function (i, name) {
+                var reformattedArray;
+                var array = [];
+                var data1 = encodeURIComponent('select * from yahoo.finance.historicaldata where symbol in ("' + name + '") and startDate = "' + startDate + '" and endDate = "' + endDate + '"');
+                $.getJSON(url, 'q=' + data1 + "&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json", function (data) {
+                    console.dir(data);
+                    var stockData = data.query.results.quote;
+                    //console.log(stockData);
+                    $.each(stockData, function( key, value ) {
+
+                        reformattedArray = $.map(value, function(ky, val) { return ky });
+
+                        //console.log(reformattedArray);
+    //                    array = reformattedArray[1];
+                        var someDate = reformattedArray[1];
+                        var date = Date.parse(someDate);
+                        //console.log(date);
+                        reformattedArray[1] = date;
+                        reformattedArray[2] = parseFloat(reformattedArray[2]);
+                        reformattedArray.shift();
+                        reformattedArray.splice(reformattedArray.length - 5);
+                        //console.log(reformattedArray);
+                        array.unshift(reformattedArray);   
+                    });
+                    //console.log(array);
+                    seriesOptions[i] = {
+                        name: name,
+                        data: array
+                    };
+
+                    // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                    // we keep a counter and create the chart when all the data is loaded.
+                    seriesCounter += 1;
+
+                    if (seriesCounter === names.length) {
+                        createChart();
+                    }
+                });
+            });
+    });
+        
+    }
+    </script>
     
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
