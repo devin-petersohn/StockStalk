e912951aaa1cd7ea1abd0a43d80b1ed3c61b7413
Sharon <xltz6@mail.missouri.edu>
2016-04-10 23:04:52 -0500

    add delete button function on the top of div on chart page

--- js/queues.js
+++ js/queues.js
@@ -1,54 +1,49 @@
-
-            var array = [];
-            var i;
-            function addToQueue(name){
-                var same = 0;
-                console.log(name.innerHTML);
-                var tickername = name.innerHTML;
-                for(i = 0; i < array.length; i ++){            
-                    if(tickername == array[i]){
-                        same = 1
-                    }
-                }
-                if(same == 0){
+var array = [];
+var i;
+function addToQueue(name){
+    var same = 0;
+    console.log(name.innerHTML);
+    var tickername = name.innerHTML;
+    for(i = 0; i < array.length; i ++){            
+        if(tickername == array[i]){
+            same = 1
+        }
+    }
+    if(same == 0){
 //                    $("#addTableRow").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername + "<div id='hideMe'><div class='deleteMe'><button><img src='img/trashcan.png'></button></div></div></input></td></tr></div>");
-                    $("#addTableRow").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername + "<button style='margin-left:8px;' type='button' class='small-btn' onclick='deleteRow(this)'><img src='img/trashcan.png'></button></td></tr>");
-                    array.push(tickername);
-                }
-                    
+        $("#addTableRow").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername + "<button style='margin-left:8px;border:0px;' type='button' class='small-btn hideMe' onclick='deleteRow(this)'><img src='img/trashcan.png' style='display:none'></button></td></tr>");
+        array.push(tickername);
+    }
+
 //                var appendString = "<tr><td><input type='checkbox' name='chartStock' value='"+tickername+"'> "+ tickername +"</input></td></tr>";
 //                console.log(appendString);
-                
-            }
+
+}
 
 
 
 //Function for adding to Navbar My Stocks//
+var arrayMS = [];
+var m;
 
-
-
-
-            var arrayMS = [];
-            var m;
-
-            function addToQueueMS(name){
-                var same = 0;
-                console.log(name.innerHTML);
-                var tickername = name.innerHTML;
-                for(m = 0; m < arrayMS.length; m ++){            
-                    if(tickername == arrayMS[m]){
-                        same = 1
-                    }
-                }
-                if(same == 0){
-                    $("#addTableRowMS").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername +"</input></td></tr>");
-                    arrayMS.push(tickername);
-                    console.log(name.innerHTML);
-                }
+function addToQueueMS(name){
+    var same = 0;
+    console.log(name.innerHTML);
+    var tickername = name.innerHTML;
+    for(m = 0; m < arrayMS.length; m ++){            
+        if(tickername == arrayMS[m]){
+            same = 1
+        }
+    }
+    if(same == 0){
+        $("#addTableRowMS").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername +"</input></td></tr>");
+        arrayMS.push(tickername);
+        console.log(name.innerHTML);
+    }
 //                var appendString = "<tr><td><input type='checkbox' name='chartStock' value='"+tickername+"'> "+ tickername +"</input></td></tr>";
 //                console.log(appendString);
-                
-            }
+
+}
 
 
 
@@ -57,18 +52,27 @@
 
 function toggle_visibility() 
 {
-//    var e = document.getElementsByClassName(hideMe);
-//    console.log(e.parentElement);
-//    if (e.style.display == 'block' || e.style.display=='')
+//    var e = document.getElementsByClassName("hideMe");
+//    console.log(e[0].firstChild);
+//    
+//       
+//    if (e[0].firstChild.style.display == 'block' || e[0].firstChild.style.display=='')
 //    {
-//        e.style.display = 'none';
+//        e[0].firstChild.style.display = 'none';
 //    }
 //    else 
 //    {
-//        e.style.display = 'block';
+//        e[0].firstChild.style.display = 'block';
 //    }
-    
-    $("hideMe").toggle();
+    $('.hideMe').each(function(i) {
+        var element = document.getElementsByClassName("hideMe");
+        var img = element[i].firstChild;
+        if (img.style.display =='none'){
+            img.style.display = 'block';
+            return;
+        }
+        img.style.display ='none';
+        });
 }
 
 
@@ -95,14 +99,14 @@ function toggle(){
 
 
 //Delete function to delete queued stock table row and also delete the element in queue array.
-            function deleteRow(btn){
-                var row = btn.parentNode.parentNode;
-                row.parentNode.removeChild(row);
-                var name = btn.parentElement.firstChild.value;
-                position = array.indexOf(name);
-                if(~position){
-                    array.splice(position,1);
-                }
-            }
+function deleteRow(btn){
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    var name = btn.parentElement.firstChild.value;
+    position = array.indexOf(name);
+    if(~position){
+        array.splice(position,1);
+    }
+}
 
 
