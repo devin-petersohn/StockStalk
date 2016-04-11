7c974b6596768b5fa58f8bd693bacd442d7d8057
Sharon <xltz6@mail.missouri.edu>
2016-04-10 22:06:24 -0500

    change delete function on chart page

--- js/queues.js
+++ js/queues.js
@@ -11,16 +11,15 @@
                     }
                 }
                 if(same == 0){
-                    $("#addTableRow").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername + "<div id='hideMe'><div class='deleteMe'><button><img src='img/trashcan.png'></button></div></div></input></td></tr></div>");
+//                    $("#addTableRow").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername + "<div id='hideMe'><div class='deleteMe'><button><img src='img/trashcan.png'></button></div></div></input></td></tr></div>");
+                    $("#addTableRow").append("<tr><td><input type='checkbox' class='chartstock' name='chartStock' value='"+tickername+"'> "+ tickername + "<button style='margin-left:8px;' type='button' class='small-btn' onclick='deleteRow(this)'><img src='img/trashcan.png'></button></td></tr>");
                     array.push(tickername);
                 }
+                    
 //                var appendString = "<tr><td><input type='checkbox' name='chartStock' value='"+tickername+"'> "+ tickername +"</input></td></tr>";
 //                console.log(appendString);
                 
             }
-
-
-
 
 
 
@@ -56,22 +55,20 @@
 //Toggle Show/Hide Delete Button
 
 
-function toggle_visibility(id) 
+function toggle_visibility() 
 {
-    /*var e = document.getElementById(id);
-    if (e.style.display == 'block' || e.style.display=='')
-    {
-        e.style.display = 'none';
-    }
-    else 
-    {
-        e.style.display = 'block';
-    }
-    */
-    $("#hideMe").toggle();
+//    var e = document.getElementsByClassName(hideMe);
+//    console.log(e.parentElement);
+//    if (e.style.display == 'block' || e.style.display=='')
+//    {
+//        e.style.display = 'none';
+//    }
+//    else 
+//    {
+//        e.style.display = 'block';
+//    }
     
-    
-    
+    $("hideMe").toggle();
 }
 
 
@@ -91,6 +88,21 @@ function toggle(){
 */
 
 //Delete function
-$(".deleteMe").on("click", function(){
-   $(this).closest("li").remove(); 
-});
+//$(".deleteMe").on("click", function(){
+//   $(this).closest("li").remove(); 
+//});
+
+
+
+//Delete function to delete queued stock table row and also delete the element in queue array.
+            function deleteRow(btn){
+                var row = btn.parentNode.parentNode;
+                row.parentNode.removeChild(row);
+                var name = btn.parentElement.firstChild.value;
+                position = array.indexOf(name);
+                if(~position){
+                    array.splice(position,1);
+                }
+            }
+
+
