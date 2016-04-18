cdaafa46e14a0e1279d6fcb1d8ccf129ea7c03c8
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-04-18 13:37:54 -0500

    Adding to setup script for more functionality.

--- setup_and_deploy/setup.sh
+++ setup_and_deploy/setup.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-echo "Please wait for packages to be installed"
-
+#Git and Maven
 sudo apt-get -y update
 sudo apt-get -y install git
 sudo apt-get -y install maven
 
+#Apache Server and relocation of web pages
 sudo apt-get -y install apache2
 
+#Jenkins and setup the daily caching of data
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get -y update
 sudo apt-get -y install jenkins
 
+#MySQL and set up the database
+sudo apt-get -y install mysql-server
+
+#Setting up StockStalk and building the source
 git clone https://github.com/devin-petersohn/StockStalk.git
 
 cd StockStalk
