b0ec4703c7e9ed0cdd192f3c08b52c85e27ae9fb
ZeningZhang <421343474@qq.com>
2016-04-18 19:57:02 -0500

    Making compatible with Java 1.7

--- One_Against_All/src/main/java/mainClass.java
+++ One_Against_All/src/main/java/mainClass.java
@@ -92,7 +92,7 @@ public class mainClass implements Serializable{
         System.out.println(stockLists);
 //         try-with-resources statement based on post comment below :)
 
-        String fileName = "tempFiles/OneVsAlltemp.json";
+        String fileName = "temp_data/OneVsAlltemp.json";
         if(new File(fileName).exists())
         {
             new File(fileName).delete();
