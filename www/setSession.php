12bb64dfc2f1c24e142b9222885d690f28cfd879
Malcolm Bouchee <mabrm9@mail.missouri.edu>
2016-04-28 21:03:53 -0500

    Database checks for replication
    Session can store google email and user’s name in mysql database as
    well.

--- www/setSession.php
+++ www/setSession.php
@@ -7,6 +7,11 @@ if(empty($_POST["googleInfo"]))
 else
 {
 	$_SESSION['username']=$_POST["googleInfo"][1];
+	$_SESSION['user']=$_POST["googleInfo"][0];
+	 $query = "INSERT INTO loginInfo (username,name) VALUES (?,?)";
+        $stmt=$dbconn->prepare($query) or die("Prepared statement error");
+        $stmt->bind_param("ssss",$_POST["googleInfo"][1],$_POST["googleInfo"][0]);
+        $stmt->execute() or die ("Execute Query failed");
 }
 
 
